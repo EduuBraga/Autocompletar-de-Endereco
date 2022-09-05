94eb5f5e0687d914220c7bb6cd72f6365ae44cec
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-09-05 14:55:23 -0300

    Corrigi o erro com o deploy

--- src/Index.jsx
+++ src/Index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import RegisterAddress from './RegisterAddress'
-import './index.css'
+import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RegisterAddress />
