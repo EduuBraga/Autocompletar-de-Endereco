370ad25be779098e1f1f161da14e3992ec19483d
Eduardo Braga <chocolate.blindado123@gmail.com>
2022-11-14 16:50:57 -0300

    Refactor: Limpando o código

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 import { Loader } from "./Loader";
 import VMasker from "vanilla-masker";
 import { Message } from "./Message";
@@ -7,58 +7,54 @@ import { Message } from "./Message";
 export function Form() {
   const { register, handleSubmit, setValue, setFocus } = useForm();
 
-  const [visibleLoader, setVisibleLoader] = useState(false)
-  const [visibleMessage, setVisibleMessage] = useState(false)
-  const [message, setMessage] = useState('')
+  const [visibleLoader, setVisibleLoader] = useState(false);
+  const [visibleMessage, setVisibleMessage] = useState(false);
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
-    VMasker(document.querySelector("#cep")).maskPattern("99999-999")
-  }, [])
+    VMasker(document.querySelector("#cep")).maskPattern("99999-999");
+  }, []);
 
   function onSubmit() {
-    setMessage('Cadastro de endereço concluído com sucesso')
-    setVisibleMessage(true)
+    setMessage('Cadastro de endereço concluído com sucesso');
+    setVisibleMessage(true);
   }
 
   const removeDisabled = () => {
-    document.querySelector("#input__1").removeAttribute('disabled')
-    document.querySelector("#input__2").removeAttribute('disabled')
-    document.querySelector("#input__3").removeAttribute('disabled')
-    document.querySelector("#input__4").removeAttribute('disabled')
-    document.querySelector("#input__5").removeAttribute('disabled')
-    document.querySelector("#input__6").removeAttribute('disabled')
+    let inputs = document.querySelectorAll('.inputs');
+
+    inputs.forEach(input => {
+      input.removeAttribute('disabled');
+    });
   }
 
   const checkCEP = (e) => {
-
     if (e.target.value.length >= 9) {
-      const cep = e.target.value.replace('-', '')
-      setVisibleLoader(true)
+      const cep = e.target.value.replace('-', '');
+      setVisibleLoader(true);
 
-      setTimeout(() => {
-        fetch(`https:/viacep.com.br/ws/${cep}/json/`)
-          .then(res => res.json())
-          .then(res => {
-            removeDisabled()
-            if (res.uf) {
-              setValue('estado', res.uf)
-              setValue('cidade', res.localidade)
-              setValue('bairro', res.bairro)
-              setValue('rua', res.logradouro)
-              setFocus('rua')
-            } else {
-              setMessage('CEP inválido, preencha o campo corretamente.')
-              setVisibleMessage(true)
-            }
-          })
-          .catch(error => console.error(error))
-          .finally(setVisibleLoader(false))
-      }, 1000)
-    } else if (e.target.value.length === 0) {
-      return ''
-    } else {
-      setMessage('CEP inválido, preencha o campo corretamente.')
-      setVisibleMessage(true)
+      fetch(`https:/viacep.com.br/ws/${cep}/json/`)
+        .then(res => res.json())
+        .then(res => {
+          removeDisabled();
+
+          if (res.uf) {
+            setValue('estado', res.uf);
+            setValue('cidade', res.localidade);
+            setValue('bairro', res.bairro);
+            setValue('rua', res.logradouro);
+            setFocus('rua');
+          } else {
+            setMessage('CEP inválido, preencha o campo corretamente.');
+            setVisibleMessage(true);
+          }
+        })
+        .catch(error => console.error(error))
+        .finally(setVisibleLoader(false))
+    }
+    else {
+      setMessage('CEP inválido, preencha o campo corretamente.');
+      setVisibleMessage(true);
     }
   }
 
@@ -83,7 +79,6 @@ export function Form() {
         <div className="row">
           <div className="form-floating col-12 col-sm-6 mb-3">
             <input
-              id="input__1"
               autoComplete="off"
               className="form-control shadow-none inputs"
               type="text"
@@ -94,9 +89,9 @@ export function Form() {
             </input>
             <label>Rua</label>
           </div>
+
           <div className="form-floating col-12 col-sm-6 mb-3">
             <input
-              id="input__2"
               className="form-control shadow-none inputs"
               type="text"
               disabled
@@ -110,7 +105,6 @@ export function Form() {
         <div className="row">
           <div className="form-floating col-12 col-sm-6 mb-3">
             <input
-              id="input__3"
               autoComplete="off"
               disabled
               required
@@ -121,9 +115,9 @@ export function Form() {
             </input>
             <label>Digite o número da residência</label>
           </div>
+
           <div className="form-floating col-12 col-sm-6 mb-3">
             <input
-              id="input__4"
               autoComplete="off"
               disabled
               required
@@ -144,15 +138,14 @@ export function Form() {
               autoComplete="off"
               disabled
               required
-              id="input__5"
               {...register("cidade")}
               placeholder="Cidade">
             </input>
             <label>Cidade</label>
           </div>
+
           <div className="form-floating col-12 col-sm-6 mb-3">
             <select
-              id="input__6"
               disabled
               autoComplete="off"
               required
@@ -194,8 +187,8 @@ export function Form() {
         <div className="d-flex justify-content-end">
           <button className="btn btn-primary shadow-none button__form">Cadastrar</button>
         </div>
-
       </form>
+
       {visibleLoader && <Loader />}
       {visibleMessage && <Message message={message} setVisibleMessage={setVisibleMessage} />}
     </div>
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,31 +1,30 @@
-export function Message(props) {
-  function handleClick(){
-    props.setVisibleMessage(false)    
-    document.querySelector("#cep").value = ''
-    document.querySelector("#input__1").value = ''
-    document.querySelector("#input__2").value = ''
-    document.querySelector("#input__3").value = ''
-    document.querySelector("#input__4").value = ''
-    document.querySelector("#input__5").value = ''
-    document.querySelector("#input__6").value = 'Estado'
+export function Message({ setVisibleMessage, message }) {
 
-    document.querySelector("#input__1").setAttribute('disabled', 'disabled')
-    document.querySelector("#input__2").setAttribute('disabled', 'disabled')
-    document.querySelector("#input__3").setAttribute('disabled', 'disabled')
-    document.querySelector("#input__4").setAttribute('disabled', 'disabled')
-    document.querySelector("#input__5").setAttribute('disabled', 'disabled')
-    document.querySelector("#input__6").setAttribute('disabled', 'disabled')
+  function handleClick() {
+    setVisibleMessage(false);
+
+    let inputs = document.querySelectorAll('.inputs');
+
+    inputs.forEach((input, index) => {
+      input.setAttribute('disabled', 'disabled');
+      input.value = '';
+
+      if (index === 5) {
+        input.value = 'Estado';
+      }
+    });
   }
 
   return (
     <div className="states">
       <div className="modal__message alert alert-dark">
         <h4>Messagem:</h4>
-        <p>{props.message}</p>
+        <p>{message}</p>
+
         <div className="d-flex justify-content-end">
-          <button onClick={()=>{handleClick()}} className="btn btn-secondary shadow-none">Fechar</button>
+          <button onClick={handleClick} className="btn btn-secondary shadow-none">Fechar</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
